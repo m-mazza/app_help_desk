933637653361028d9e6d35f59c0957dd2ba352f5
Marcus Mazza <m-mazza@live.com>
2026-01-29 23:47:29 -0300

    deletando arquivos que não serão usados mais

--- validation.php
+++
@@ -1,9 +0,0 @@
-<?php
-  
-    require_once 'functions/function_geral.php';
-    require_once 'functions/function_user.php'; 
-      
-    if($_SERVER['REQUEST_METHOD'] == 'POST')
-    {
-        
-?>
